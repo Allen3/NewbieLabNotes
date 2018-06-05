34f5200cde71c070a3233674e1dbf9e7b8d7ca6c
Allen3 <Allen.3.ysj@gmail.com>
2018-06-05 19:46:50 +0800

    update($web): comment in demo added. - add comment to help explain the callback usage

--- tests/nodejs/flow_control/index.js
+++ tests/nodejs/flow_control/index.js
@@ -2,30 +2,35 @@
 
 const flow = require('nimble');
 
-let callback = function() {
+let callback_f = function() {
     console.log("I'm the callback function!");
 };
 
 flow.series([
+    /**
+     * According to unofficial doc, the callback here is used to start the next task
+     */
     (callback) => {        
-        setTimeout(() => {
+        setTimeout(() => {            
             console.log('I execute first.');
-            callback();
-        }, 1000);
+        }, 2000);
+        callback();
     },
 
     (callback) => {
         setTimeout(() => {
             console.log('I execute next.');
             callback();
-        }, 500);
+        }, 1000);
     },
 
     (callback) => {
         setTimeout(() => {
             console.log('I execute last.');
             callback();
-        }, 100);
+        }, 3000);
     }
-]);
+], () => {
+    console.log('Flow.series successfully ends.');
+});
 
